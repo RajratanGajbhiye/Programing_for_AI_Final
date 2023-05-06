c59fe9eac10913ffd7e81b069fcf92b61dd57a87
RajratanGajbhiye <rajratan.gajbhiye.career@gmail.com>
2023-05-06 18:35:47 +0100

    removved redundant code

--- main.py
+++ main.py
@@ -318,13 +318,13 @@ def deleteProductFromCart(code):
     return "Error occurred while deleting"
 
 
-def array_merge(first_array, second_array):
-    if isinstance(first_array, list) and isinstance(second_array, list):
-        return first_array + second_array
-    elif isinstance(first_array, dict) and isinstance(second_array, dict):
-        return dict(list(first_array.items()) + list(second_array.items()))
-    elif isinstance(first_array, set) and isinstance(second_array, set):
-        return first_array.union(second_array)
+def array_merge(firstArray, secondArray):
+    if isinstance(firstArray, list) and isinstance(secondArray, list):
+        return firstArray + secondArray
+    elif isinstance(firstArray, dict) and isinstance(secondArray, dict):
+        return dict(list(firstArray.items()) + list(secondArray.items()))
+    elif isinstance(firstArray, set) and isinstance(secondArray, set):
+        return firstArray.union(secondArray)
     return False
 
 
